2cfe55409ef37809153714a6940b0a73dab29fa3
haroldherrera <haroldherrera@gmail.com>
2025-07-01 21:36:30 -0500

    add shadcn and tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 /// <reference types="vitest/config" />
-// import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 
   test: {
     globals: true,
