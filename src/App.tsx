b0209cb59cfeeaa4a4becbdbfdf51d3bf7acbdc6
haroldherrera <haroldherrera@gmail.com>
2025-07-02 22:32:18 -0500

    Add basic actions to get statrted

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div className='flex min-h-svh flex-col items-center justify-center'>
-        <Button>Click me!</Button>{' '}
+        <Button>Click me now!</Button>
       </div>
       <div>
         <a href='https://vite.dev' target='_blank'>
