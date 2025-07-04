e01c8e26e2bf357684b3d214d62cbb10d1f8858b
haroldherrera <haroldherrera@gmail.com>
2025-07-03 19:18:06 -0500

    Change the mesage of the home page

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <Button>Click me again!</Button>
+      <Button>Click me 1!</Button>
       <div>
         <a href='https://vite.dev' target='_blank'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
